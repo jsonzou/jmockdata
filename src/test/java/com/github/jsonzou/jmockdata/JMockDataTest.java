f866d1676adb26ef8e8ee1d639e1fb631c491618
贺威 <wei.he@pds-inc.com.cn>
2018-11-26 12:09:31 +0800

    1.排除mock配置 2.部分mock配置

--- src/test/java/com/github/jsonzou/jmockdata/JMockDataTest.java
+++ src/test/java/com/github/jsonzou/jmockdata/JMockDataTest.java
@@ -158,17 +158,20 @@ public class JMockDataTest {
             .charSeed((char) 97, (char) 98)
             .byteRange((byte) 0, Byte.MAX_VALUE)
             .shortRange((short) 0, Short.MAX_VALUE)
+
             // 某些字段（名等于integerNum的字段、包含float的字段、double开头的字段）配置
             .subConfig("integerNum","*float*","double*")
             .intRange(10, 11)
             .floatRange(1.22f, 1.50f)
             .doubleRange(1.50,1.99)
+
             // 某个类的某些字段（long开头的字段、date结尾的字段、包含string的字段）配置。
             .subConfig(BasicBean.class,"long*","*date","*string*")
             .longRange(12, 13)
             .dateRange("2018-11-20", "2018-11-30")
             .stringSeed("SAVED", "REJECT", "APPROVED")
             .sizeRange(1,1)
+
             // 全局配置
             .globalConfig()
             // 排除所有包含list/set/map字符的字段。表达式不区分大小写。
