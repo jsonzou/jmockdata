c01303ae0411c9fab315be9fa0ede04685c0cf5a
JsonZou <keko-boy@163.com>
2020-12-03 19:24:17 +0800

    1.删除误用注释

--- src/test/java/com/github/jsonzou/jmockdata/bean/SimpleBean.java
+++ src/test/java/com/github/jsonzou/jmockdata/bean/SimpleBean.java
@@ -1,17 +1,9 @@
-/**
- * *****************************************************
- * Copyright (C) 2020 pds-inc.com.cn. All Rights Reserved
- * This file is part of pds project.
- * Unauthorized copy of this file, via any medium is strictly prohibited.
- * Proprietary and Confidential.
- * ****************************************************
- **/
 package com.github.jsonzou.jmockdata.bean;
 
 import lombok.Setter;
 
 /**
- * @author jsonzou<pengfei.zou @ pds-inc.com.cn>
+ * @author jsonzou
  * @date 12/02/2020 18:33
  */
 public class SimpleBean {
