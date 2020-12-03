0ff3e97115f4b86611155754674963deed3ba735
JsonZou <keko-boy@163.com>
2020-12-03 19:19:33 +0800

    1.增加bean mocker拦截器支持 2.增加支持配置Modifier:static、public、protected、private 3.排除final修饰的字段 4.对可模拟的class类做一些过滤

--- src/main/java/com/github/jsonzou/jmockdata/BeanMockerInterceptor.java
+++ src/main/java/com/github/jsonzou/jmockdata/BeanMockerInterceptor.java
@@ -19,7 +19,7 @@ public interface BeanMockerInterceptor<T>{
      * 返回值类型说明：
      *   - InterceptType.UNMOCK 不模拟：排除此字段的模拟
      *   - InterceptType.MOCK 自动模拟
-     *   - 其他返回值，作为field的值通过反射注入
+     *   - 其他返回值，作为Field的值通过反射注入
      * @param clazz
      * @param field
      * @param bean
